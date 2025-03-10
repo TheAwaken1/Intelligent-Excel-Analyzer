9dd2ed36fb8dfae028572890d25a4f09b6fee6e6
TheAwaken1 <cmpcreativedesigns@gmail.com>
2025-03-09 17:53:26 -0700

    updated start.js file to capture Gradio URL and auto-open browser

--- start.js
+++ start.js
@@ -1,21 +1,25 @@
 module.exports = {
-  daemon: true,
-  run: [
+  "daemon": true,
+  "run": [
     {
-      method: "shell.run",
-      params: {
-        venv: "env",
-        path: "app",
-        message: "python app.py",
-        env: {
-          HF_TOKEN: "{{secrets.HF_TOKEN}}"
-        },
-        on: [{ event: "/http:\\/\\/\\S+/", done: true }]
+      "method": "shell.run",
+      "params": {
+        "venv": "env",
+        "path": "app",
+        "message": "python app.py",
+        "on": [{
+          "event": "/Running on local URL:\\s*(http:\\/\\/[^\\s]+)/",
+          "done": true
+        }]
       }
     },
     {
-      method: "local.set",
-      params: { url: "{{input.event[0]}}" }
+      "method": "local.set",
+      "params": { "url": "{{input.event[0]}}" }
+    },
+    {
+      "method": "browser.open",
+      "params": { "uri": "{{input.event[0]}}" }
     }
   ]
-};
+}
